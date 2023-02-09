2a9a30dc5e7fd85f88d88d2563cec30f723e0f4d
Matteo Carbone <mc@matteocarbone.com>
2023-02-09 13:16:14 +0100

    fix(questions): dev URL for email notifications

--- back-end/src/handlers/answers.ts
+++ back-end/src/handlers/answers.ts
@@ -23,7 +23,7 @@ const DDB_TABLES = {
 };
 const ddb = new DynamoDB();
 
-const QUESTION_BASE_URL = 'https://esn-ga.link/t/topics/';
+const QUESTION_BASE_URL = `https://${process.env.STAGE === 'prod' ? '' : 'dev.'}esn-ga.link/t/topics/`;
 const SES_CONFIG = {
   sourceName: 'ESN General Assembly Q&A',
   source: process.env.SES_SOURCE_ADDRESS,
