be5cb5ff7f25b1e85d579ad9ead3b4c7f2417d29
Matteo Carbone <mc@matteocarbone.com>
2023-03-27 10:10:29 +0100

    fix(users): spaces in URL to ESN Accounts profile

--- back-end/src/models/utils.ts
+++ back-end/src/models/utils.ts
@@ -1,4 +1,4 @@
 /**
  * We need to strip weird characters to build a valid ID to open the entity on ESN Accounts.
  */
-export const cleanESNAccountsIdForURL = (id: string): string => id.replace(/[._]/gm, '');
+export const cleanESNAccountsIdForURL = (id: string): string => id.replace(/[._]/gm, '').replace(/\s/gm, '-');
