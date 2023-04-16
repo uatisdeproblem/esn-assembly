3ac04cde0ca7703c0d6b8429b7ba5a011cbccdc1
Matteo Carbone <mc@matteocarbone.com>
2023-04-16 15:01:00 +0200

    fix: change date comparison granularity to minutes #20

--- back-end/src/models/favoriteTimezone.const.ts
+++ back-end/src/models/favoriteTimezone.const.ts
@@ -3,6 +3,9 @@ import { epochISODateString } from 'idea-toolbox';
 export const FAVORITE_TIMEZONE = 'Europe/Brussels';
 export const GMT_0_TIMEZONE = 'Europe/Dublin';
 
+/**
+ * Convert the date string in the favorite timezone (granularity: minutes).
+ */
 export const getDateStringInFavoriteTimezone = (date: Date | epochISODateString, timezone: string): string => {
   const sortableFormattingLocale = 'sv-SE';
   const formatter = new Intl.DateTimeFormat(sortableFormattingLocale, {
@@ -11,17 +14,22 @@ export const getDateStringInFavoriteTimezone = (date: Date | epochISODateString,
     day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit',
     timeZone: timezone
   });
   return formatter.format(new Date(date));
 };
 
+/**
+ * Whether the date is in the future (granularity: minutes).
+ */
 export const dateStringIsFuture = (dateString: epochISODateString, timezone: string): boolean => {
   const now = getDateStringInFavoriteTimezone(new Date(), timezone);
   return !dateString || now < getDateStringInFavoriteTimezone(dateString, timezone);
 };
 
+/**
+ * Whether the date is in the past (granularity: minutes).
+ */
 export const dateStringIsPast = (dateString: epochISODateString, timezone: string): boolean => {
   const now = getDateStringInFavoriteTimezone(new Date(), timezone);
   return !dateString || now > getDateStringInFavoriteTimezone(dateString, timezone);
