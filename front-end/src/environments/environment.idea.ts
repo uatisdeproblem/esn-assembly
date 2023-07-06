1198c9c946bcb7eb072be8a3bb1e53ebd3fcea1f
Matteo Carbone <mc@matteocarbone.com>
2023-07-06 18:13:53 +0200

    v1.1.9

--- front-end/src/environments/environment.idea.ts
+++ front-end/src/environments/environment.idea.ts
@@ -5,7 +5,7 @@ export const environment = {
   idea: {
     project: 'esn-ga',
     app: {
-      version: '1.1.8',
+      version: '1.1.9',
       url: 'https://qa.esn.org',
       mediaUrl: 'https://media.esn-ga.link',
       title: 'ESN General Assembly Q&A',
