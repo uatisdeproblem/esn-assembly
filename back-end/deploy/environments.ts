c9a5cca8bedc3a0898a4be944beaf1f93b140441
Matteo Carbone <mc@matteocarbone.com>
2024-02-21 20:02:38 +0000

    chore: config for ESN Lithuania

--- back-end/deploy/environments.ts
+++ back-end/deploy/environments.ts
@@ -1,15 +1,15 @@
 /**
  * The codename of the project.
  */
-export const PROJECT = 'esn-ga';
+export const PROJECT = 'esn-assembly-lt';
 /**
  * The purchased domain to use.
  */
-export const DOMAIN = 'esn-ga.link';
+export const DOMAIN = 'esn-assembly-lt.link';
 /**
  * An additional custom domain to use.
  */
-export const PROD_CUSTOM_DOMAIN: string | null = 'ga.esn.org'; // in case of first creation, use: `null`
+export const PROD_CUSTOM_DOMAIN: string | null = null; // in case of first creation, use: `null`
 
 export const parameters: Parameters = {
   project: PROJECT,
--- front-end/release.sh
+++ front-end/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # project specific parameters
-AWS_PROFILE='esn-ga'
-DOMAIN_PROD='esn-ga.link'
-DOMAIN_DEV='dev.esn-ga.link'
+AWS_PROFILE='esn-assembly-lt'
+DOMAIN_PROD='esn-assembly-lt.link'
+DOMAIN_DEV='dev.esn-assembly-lt.link'
 
 # other parameters
 ACTION=$1
